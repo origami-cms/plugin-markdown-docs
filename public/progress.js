8d0da6b8c220a774161a9f54128b8fcd67a74339
Tristan Matthias <hello@tristanmatthias.com>
2018-09-07 12:20:24 +1000

    Added complete state to progress

--- public/progress.js
+++ public/progress.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
-    const canvas = document.getElementById('progress');
+    const progress = document.getElementById('progress');
+    const canvas = progress.querySelector('canvas');
     if (!canvas) return;
 
     const ctx = canvas.getContext('2d');
@@ -11,16 +12,21 @@ window.addEventListener('load', () => {
         if (perc > 1) perc = 1;
 
         ctx.clearRect(0,0,40,40);
-        ctx.lineWidth = 4;
-        ctx.beginPath();
-        ctx.arc(20, 20, 17, 0, 2 * Math.PI);
-        ctx.strokeStyle = '#eae3f0';
-        ctx.stroke();
+        ctx.lineWidth = 2;
+
+        if (perc < 1) {
+            ctx.beginPath();
+            ctx.arc(20, 20, 17, 0, 2 * Math.PI);
+            ctx.strokeStyle = '#eae3f0';
+            ctx.stroke();
+        }
 
         ctx.beginPath();
         ctx.arc(20, 20, 17, 0, 2 * Math.PI * perc);
         ctx.strokeStyle = '#693a91';
         ctx.stroke();
+
+        if (perc === 1) progress.classList.add('complete', perc === 1);
     }
 
     updateProgress();
